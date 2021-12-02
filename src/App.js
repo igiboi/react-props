ff3a5503b85a8f6ec26886523e4e92784cc9a8ae
Luigi Matias <36388765+igiboi@users.noreply.github.com>
2021-12-02 13:13:01 -0500

    added date page and styling

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
+      title: "New Desk(Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
